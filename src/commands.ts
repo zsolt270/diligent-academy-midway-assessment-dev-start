4adaf7a459a8eb7180bc76afa3ee34ebaed5af7d
zsolt270 <zsolt270000@gmail.com>
2024-10-21 16:37:40 +0200

    added changes to the list command, now the user cant give arguments to the command. It will throw an error msg

--- src/commands.ts
+++ src/commands.ts
@@ -2,11 +2,15 @@ import { Recipe, RecipeType } from "./recipe";
 import { Store } from "./stores/store.type";
 
 export async function list(store: Store<RecipeType[]>, args: string[]) {
-  const recipe = new Recipe(store);
-  const recipes = await recipe.readAll();
-  const formatted = recipes
-    .map((recipe) => `- [${recipe.id}] ${recipe.name}`)
-    .join('\n');
-  console.log('Your recipes:');
-  console.log(formatted);
+	if (args.length != 0) {
+		console.log("Error: The list command should not have any argument.");
+		return;
+	}
+	const recipe = new Recipe(store);
+	const recipes = await recipe.readAll();
+	const formatted = recipes
+		.map((recipe) => `- [${recipe.id}] ${recipe.name}`)
+		.join("\n");
+	console.log("Your recipes:");
+	console.log(formatted);
 }
